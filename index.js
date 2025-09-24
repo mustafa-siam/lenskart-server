0132ab162ac800f11f3bd8c2269283100eb0e938
mustafa siam <mustafasiam357@gmail.com>
2025-09-25 00:20:50 +0600

    filter query added

--- index.js
+++ index.js
@@ -30,14 +30,53 @@ async function run() {
     // Send a ping to confirm a successful connection
     const cartcolloection=client.db('lenskart').collection('carts')
     const ordercolloection=client.db('lenskart').collection('orders')
-    app.get('/allitems',async(req,res)=>{
-      const result=await cartcolloection.find().toArray()
-      res.send(result)
-    })
+    const orderhistorycolloection=client.db('lenskart').collection('order history')
+    const favcartcollection=client.db('lenskart').collection('whislist')
+   app.get('/allitems', async (req, res) => {
+        const { category, shape, search, sort } = req.query;
+        let query = {};
+        
+        if (category) {
+            query.category = category;
+        }
+        if (shape) {
+            query.shape = shape;
+        }
+        if (search) {
+            query.$or = [
+                { name: { $regex: search, $options: 'i' } },
+                { category: { $regex: search, $options: 'i' } }
+            ];
+        }
+
+        let sortOptions = {};
+        if (sort === 'low_to_high') {
+            sortOptions.price = 1;
+        } else if (sort === 'high_to_low') {
+            sortOptions.price = -1;
+        }
+
+        const result = await cartcolloection.find(query).sort(sortOptions).toArray();
+        res.send(result);
+    });
+
+
     app.get('/allitems/:id',async(req,res)=>{
       const id=req.params.id;
       const query={_id:new ObjectId(id)}
       const result=await cartcolloection.findOne(query)
+      res.send(result)
+    })
+    app.patch('/allitems/:id',async(req,res)=>{
+      const id=req.params.id;
+      const item=req.body;
+      const query={_id:new ObjectId(id)}
+      const updetdoc={
+        $set:{
+           favcart:item.favcart,
+        }
+      }
+      const result=await cartcolloection.updateOne(query,updetdoc)
       res.send(result)
     })
     app.post('/allorders',async(req,res)=>{
@@ -68,6 +107,27 @@ async function run() {
       },
       }
       const result=await ordercolloection.updateOne(filter,updatedoc)
+      res.send(result)
+    })
+     app.post('/orderhistory',async(req,res)=>{
+      const orderdata=req.body;
+      const result=await orderhistorycolloection.insertOne(orderdata);
+      for(const item of orderdata.orders){
+        const filter={_id:new ObjectId(item.cartid)}
+        const update={
+          $inc:{quantity:-item.orderqty}
+        }
+        await cartcolloection.updateOne(filter,update)
+      }
+      const email=orderdata.user;
+      const filter={email:email};
+      await ordercolloection.deleteMany(filter)
+      res.send(result)
+    })
+     app.get('/orderhistory/:orderid',async(req,res)=>{
+      const id=req.params.orderid;
+      const query={_id:new ObjectId(id)}
+      const result=await orderhistorycolloection.findOne(query);
       res.send(result)
     })
     await client.db("admin").command({ ping: 1 });
